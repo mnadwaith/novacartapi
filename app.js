fa20caaec2a004fe27dc7f2a23c9a553b130cd25
Adwaith M Nambiar <mnadwaith@gmail.com>
2024-08-22 18:05:56 +0530

    all api

--- app.js
+++ app.js
@@ -6,130 +6,130 @@ let cors = require('cors');
 
 app.use(cors())
 
-let {dbConnect,getData} = require('./controller/dbController');
+let { dbConnect, getData } = require('./controller/dbController');
 
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send("This is the root");
 });
 
-app.get('/banner',async(req,res)=>{
+app.get('/banner', async (req, res) => {
     let query = {};
     let collection = "banner";
-    let output = await getData(collection,query);
+    let output = await getData(collection, query);
     res.send(output);
 });
 
-app.get('/catagories',async(req,res)=>{
+app.get('/catagories', async (req, res) => {
     let query = {};
     let collection = "catagories";
-    let output = await getData(collection,query);
+    let output = await getData(collection, query);
     res.send(output);
 });
 
-app.get('/items',async(req,res)=>{
+app.get('/items', async (req, res) => {
     let query = {};
     let collection = "items";
     let lcost = Number(req.query.lcost)
     let hcost = Number(req.query.hcost)
-    if(lcost & hcost){
-        query={
-            $and:[{price:{$gt:lcost,$lt:hcost}}]
+    if (lcost & hcost) {
+        query = {
+            $and: [{ price: { $gt: lcost, $lt: hcost } }]
         }
     }
-    let output = await getData(collection,query);
+    let output = await getData(collection, query);
     res.send(output);
 });
 
 
 
-app.get('/electronics',async(req,res)=>{
+app.get('/electronics', async (req, res) => {
     let query = {};
     let collection = "electronics";
     let lcost = Number(req.query.lcost)
     let hcost = Number(req.query.hcost)
-    if(lcost & hcost){
-        query={
-            $and:[{price:{$gt:lcost,$lt:hcost}}]
+    if (lcost & hcost) {
+        query = {
+            $and: [{ price: { $gt: lcost, $lt: hcost } }]
         }
     }
-    let output = await getData(collection,query);
+    let output = await getData(collection, query);
     res.send(output);
 });
 
-app.get('/fashion',async(req,res)=>{
+app.get('/fashion', async (req, res) => {
     let query = {};
     let collection = "fashion";
     let lcost = Number(req.query.lcost)
     let hcost = Number(req.query.hcost)
-    if(lcost & hcost){
-        query={
-            $and:[{price:{$gt:lcost,$lt:hcost}}]
+    if (lcost & hcost) {
+        query = {
+            $and: [{ price: { $gt: lcost, $lt: hcost } }]
         }
     }
-    let output = await getData(collection,query);
+    let output = await getData(collection, query);
     res.send(output);
 });
 
-app.get('/home_kitchen',async(req,res)=>{
+app.get('/home_kitchen', async (req, res) => {
     let query = {};
     let collection = "home_kitchen";
     let lcost = Number(req.query.lcost)
     let hcost = Number(req.query.hcost)
-    if(lcost & hcost){
-        query={
-            $and:[{price:{$gt:lcost,$lt:hcost}}]
+    if (lcost & hcost) {
+        query = {
+            $and: [{ price: { $gt: lcost, $lt: hcost } }]
         }
     }
-    let output = await getData(collection,query);
+    let output = await getData(collection, query);
     res.send(output);
 });
 
-app.get('/sports_outdoor',async(req,res)=>{
+app.get('/sports_outdoor', async (req, res) => {
     let query = {};
     let collection = "sports_outdoor";
     let lcost = Number(req.query.lcost)
     let hcost = Number(req.query.hcost)
-    if(lcost & hcost){
-        query={
-            $and:[{price:{$gt:lcost,$lt:hcost}}]
+    if (lcost & hcost) {
+        query = {
+            $and: [{ price: { $gt: lcost, $lt: hcost } }]
         }
     }
-    let output = await getData(collection,query);
+    let output = await getData(collection, query);
     res.send(output);
 });
 
-app.get('/toys_games',async(req,res)=>{
+app.get('/toys_games', async (req, res) => {
     let query = {};
     let collection = "toys_games";
     let lcost = Number(req.query.lcost)
     let hcost = Number(req.query.hcost)
-    if(lcost & hcost){
-        query={
-            $and:[{price:{$gt:lcost,$lt:hcost}}]
+    if (lcost & hcost) {
+        query = {
+            $and: [{ price: { $gt: lcost, $lt: hcost } }]
         }
     }
-    let output = await getData(collection,query);
+    let output = await getData(collection, query);
     res.send(output);
 });
 
-app.get('/beauty_personal_care',async(req,res)=>{
+app.get('/beauty_personal_care', async (req, res) => {
     let query = {};
     let collection = "beauty_presonal_care";
     let lcost = Number(req.query.lcost)
     let hcost = Number(req.query.hcost)
-    if(lcost & hcost){
-        query={
-            $and:[{price:{$gt:lcost,$lt:hcost}}]
+    if (lcost & hcost) {
+        query = {
+            $and: [{ price: { $gt: lcost, $lt: hcost } }]
         }
     }
-    let output = await getData(collection,query);
+    let output = await getData(collection, query);
     res.send(output);
 });
 
 
-app.listen(port,(err)=>{
+app.listen(port, (err) => {
     dbConnect();
-    if(err) throw err;
+    if (err) throw err;
     console.log(`Server is running on port ${port}`);
 });
